e4056c34e5e00c2096a3e0b7152b7d2305728d04
Younus-Saberi <younussaberi@gmail.com>
2024-08-12 17:52:58 +0530

    Add @vercel/analytics for website insights

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ import "./css/card.scss";
 import "./css/globals.scss";
 import ScrollToTop from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: "Portfolio of Younus Saberi- Software Developer",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
+          <Analytics/>
           <ScrollToTop />
         </main>
         <Footer />
