e552e57a4be467fb8ef27745b06260f774c4b6ee
Younus-Saberi <younussaberi@gmail.com>
2025-01-09 04:50:35 +0000

    update experience section 🏢

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,18 +1,24 @@
 export const experiences = [
   {
     id: 1,
+    title: 'Software Engineer',
+    company: "Delta Electronics",
+    duration: "(Oct 2024 - Present)"
+  },
+  {
+    id: 2,
     title: 'Backend Developer Intern',
     company: "Trade Brains",
     duration: "(Feb 2024 - June 2024)"
   },
   {
-    id: 2,
+    id: 3,
     title: "Software Developer Intern",
     company: "EMotorad",
     duration: "(Aug 2023 - Nov 2023)"
   },
   {
-    id: 3,
+    id: 4,
     title: "F13 Technologies",
     company: "Cloud Engineer",
     duration: "(Jan 2023 - April 2023)"
